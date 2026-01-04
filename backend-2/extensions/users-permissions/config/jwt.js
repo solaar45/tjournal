6aedcd4c5b16fbfa375bd7dcb13518f3cf66acfd
solaar45 <solaar45@yahoo.de>
2026-01-04 13:04:19 +0100

    chore: Remove obsolete backend folders - using Next.js API routes instead

--- backend-2/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '7b7866ce-8079-47c5-b3c8-f0bc45e2896f'
-};
--- backend_phoenix/lib/tjournal/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Tjournal.Repo do
-  use Ecto.Repo,
-    otp_app: :tjournal,
-    adapter: Ecto.Adapters.Postgres
-end
